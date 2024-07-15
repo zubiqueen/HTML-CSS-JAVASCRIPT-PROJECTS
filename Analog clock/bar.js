138b582947bafec73294da72dafd9a0b65534341
Zubda Naveed <zubi41796@gmail.com>
2024-07-15 18:38:07 +0500

    Delete Analog clock/bar.js

--- Analog clock/bar.js
+++
@@ -1,9 +0,0 @@
-// JavaScript for dropdown menu
-document.querySelectorAll('.navbar > a').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.nextElementSibling.style.display = 'block';
-    });
-    item.addEventListener('mouseout', () => {
-        item.nextElementSibling.style.display = 'none';
-    });
-});
